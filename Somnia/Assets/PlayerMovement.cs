37f44f8d7153d103e5aa2a77768aa82769ece14d
Morgan Alger <malger2@charlotte.edu>
2026-05-08 19:13:18 -0400

    Player Movement (Idle -> Walk)

---
+++ Somnia/Assets/PlayerMovement.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 0.05f;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sr;
+
+    private float moveInput;
+
+    public float jumpForce = 5f;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+        sr = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+        // ground check
+        isGrounded = Physics2D.OverlapCircle(
+            groundCheck.position,
+            groundCheckRadius,
+            groundLayer
+        );
+
+        // animation speed
+        anim.SetFloat("Speed", Mathf.Abs(moveInput));
+
+        // flip character direction
+        if (moveInput < 0)
+            sr.flipX = false;   // D = right
+        else if (moveInput > 0)
+            sr.flipX = true;    // A = left
+
+        // jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = Vector2.Lerp(
+            rb.velocity,
+            new Vector2(moveInput * moveSpeed, rb.velocity.y),
+            0.15f
+        );
+    }
+}
