ae8e7c565f586999ae9bdbb4dc218d2daf493707
Morgan Alger <malger2@charlotte.edu>
2026-05-08 22:47:21 -0400

    Jump / Double Jump Mechanic

--- Somnia/Assets/PlayerMovement.cs
+++ Somnia/Assets/PlayerMovement.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 0.01f;
+    public float moveSpeed = 3f;
+    public float jumpForce = 12f;
 
     private Rigidbody2D rb;
     private Animator anim;
@@ -10,7 +11,6 @@ public class PlayerMovement : MonoBehaviour
 
     private float moveInput;
 
-    public float jumpForce = 5f;
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
@@ -27,31 +27,27 @@ public class PlayerMovement : MonoBehaviour
     {
         moveInput = Input.GetAxisRaw("Horizontal");
 
-        // ground check
+        anim.SetFloat("Speed", Mathf.Abs(moveInput));
+
+        if (moveInput < 0)
+            sr.flipX = false;
+        else if (moveInput > 0)
+            sr.flipX = true;
+
+        // DEBUG JUMP TEST
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * 12f;
+        }
+    }
+    void FixedUpdate()
+    {
         isGrounded = Physics2D.OverlapCircle(
             groundCheck.position,
             groundCheckRadius,
             groundLayer
         );
 
-        // animation speed
-        anim.SetFloat("Speed", Mathf.Abs(moveInput));
-
-        // flip character direction
-        if (moveInput < 0)
-            sr.flipX = false;   // D = right
-        else if (moveInput > 0)
-            sr.flipX = true;    // A = left
-
-        // jump
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
-    }
-
-    void FixedUpdate()
-    {
-        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);;
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
     }
 }
